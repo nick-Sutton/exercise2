5792d7f563029b551a718d2980e60b7514ea8290
Nicholas Sutton <nicksutton46@gmail.com>
2026-08-24 14:34:17 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -182,7 +182,7 @@ function main() {
     // do the interpolation
     for (var y=uly; y<=lly; y++) {
         hc.copy(lc); // begin with the left color
-        var slope = (uly - lry) / (ulx - lrx)
+        var slope = Math.ceil((uly - lry) / (ulx - lrx))
         hcDelta.copy(rc).subtract(lc).scale(hDelta); // reset horiz color delta
         for (var x=ulx; x<=slope; x++) {
             drawPixel(imagedata,x,y,hc);   
